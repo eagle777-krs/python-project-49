152e7a7f1ff26104a024629b455b62159db789d4
eagle777-krs <williamgarfield67@gmail.com>
2025-01-02 14:45:29 +0300

    changing brain_even structure

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,24 +1,7 @@
-from random import randint
-from brain_games.cli import welcome_user
+from brain_games.games.even_game import brain_even_game
 
-point_tracker = 0
-Flag = True
-name = welcome_user()
+def main():
+	brain_even_game()
 
-while Flag == True:
-    number = randint(1, 100)
-    print(f'Answer "yes" if the number is even, otherwise answer "no".\nQuestion: {number}')
-    answer = input('Your answer: ')
-
-    right_answer = 'yes' if number % 2 == 0 else 'no'
-
-    if right_answer == answer:
-        point_tracker += 1
-        print('Correct!')
-        if point_tracker == 3:
-            print(f'Congratulations, {name}!')
-            Flag = False
-
-    else:
-        print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.\nLet's try again, {name}!")
-        Flag = False
+if __name__ == '__main__':
+	main()
